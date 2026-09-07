0ba30bcbabddd3f31e8bf422a80a1835a5a6b8ba
Drtikolnik <drtluk00@gasos-ro.cz>
2026-09-07 10:29:59 +0200

    sdwcwegwg

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("Joj", 2, 1.0);
-        Student student2 = new Student("Jane", 3, 5.0);
-        Student student3 = new Student("Joj", 4, 1.0);
 
-        System.out.println(student);
 
-        System.out.println(student.equals(student3));
 
-        System.out.println(student2);
-
-        student2.zlepsiPrumer(2.0);
-
-        System.out.println(student2);
-
-        System.out.println(student3);
-        student3.klasifikace();
     }
 }

35298ad7c79165f7910458695e4a9f00c6fc77ca
Drtikolnik <drtluk00@gasos-ro.cz>
2026-09-07 11:35:48 +0200

    sdwcwegwguk

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
+        SkolniSystem ss = new SkolniSystem();
+
     for(;;){
         System.out.println("MENU");
         System.out.println("1 - přidat studenta");
@@ -13,13 +15,24 @@ public class Main {
 
         switch (vyber){
             case 1:
-
+                    System.out.println("zadej jméno");
+                    String jmeno = sc.nextLine();
+                    System.out.println("zadej rocnik");
+                    int rocnik = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("zadej prumer");
+                    double prumer = sc.nextDouble();
+                    sc.nextLine();
+                    ss.pridatStudenta(new Student(jmeno, rocnik, prumer));
                 break;
 
             case 2:
+                    ss.vypsatStudenty();
                 break;
 
             case 3:
+                System.out.println("zadej jméno");
+                ss.najitDleJmena(sc.nextLine());
                 break;
         }
     }

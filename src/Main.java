3d302ad32c7a34dfff9f77d719df968d6543dd56
Drtikolnik <drtluk00@gasos-ro.cz>
2026-09-07 10:52:14 +0200

    sdwcwegwguk

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+
+    for(;;){
+        System.out.println("MENU");
+        System.out.println("1 - přidat studenta");
+        System.out.println("2 - vypsat studenty");
+        System.out.println("3 - hledat podle jména");
+        int vyber = sc.nextInt();
+        sc.nextLine();
+
+        switch (vyber){
+            case 1:
+
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 

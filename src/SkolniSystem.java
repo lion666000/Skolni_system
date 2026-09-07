33355839e5d687fe66d2ae944b28f408c12a480b
Drtikolnik <drtluk00@gasos-ro.cz>
2026-09-07 10:39:20 +0200

    sdwcwegwguk

--- src/SkolniSystem.java
+++ src/SkolniSystem.java
@@ -1,23 +1,3 @@
-import java.util.ArrayList;
-
 public class SkolniSystem {
-    ArrayList<Student> students = new ArrayList<>();
 
-    public void pridatStudenta(Student student){
-        students.add(student);
-    }
-
-    public void vypsatStudenty(){
-        for(Student student : students){
-            System.out.println(student);
-        }
-    }
-
-    public void najitDleJmena(String jmeno){
-        for(Student student : students){
-            if(student.getJmeno().equals(jmeno)){
-                System.out.println(student);
-            }
-        }
-    }
 }
